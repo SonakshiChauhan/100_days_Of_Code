6d3bbe2d3a5f895e661537da09f73c89ad496e9a
Sonakshi Chauhan <chauhansonakshi1402@gmail.com>
2022-09-06 21:17:49 +0530

    .

---
+++ src/com/company/reversewordsinastring.java
@@ -0,0 +1,37 @@
+package com.company;
+/*
+Runtime:161ms
+Space:133.7 MB
+ */
+
+public class reversewordsinastring {
+    public static String rev(String s) {
+        char[] a = s.toCharArray();
+        for (int i = 0, j = a.length - 1; i < a.length && i < j; i++, j--) {
+            char temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+        String ans = "";
+        for (char c : a) {
+            ans += c;
+        }
+        return ans;
+    }
+
+    public static String reverseWords(String s) {
+        String[] a = s.split(" ");
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = rev(a[i]);
+        }
+        String ans = "";
+        for (int i = 0; i < a.length; i++) {
+            ans += a[i];
+            if (i < a.length - 1) ans += " ";
+        }
+
+        return ans;
+
+    }
+}
