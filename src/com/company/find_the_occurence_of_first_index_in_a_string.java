c9121a7be1d3647c25bbf39e79b09b53e9631dc2
Sonakshi Chauhan <chauhansonakshi1402@gmail.com>
2022-09-07 22:47:43 +0530

    .

---
+++ src/com/company/find_the_occurence_of_first_index_in_a_string.java
@@ -0,0 +1,11 @@
+package com.company;
+/*
+Runtime-0ms
+Space-42.1 MB
+ */
+public class find_the_occurence_of_first_index_in_a_string {
+    public int strStr(String haystack, String needle) {
+        int i = haystack.indexOf(needle);
+        return i;
+    }
+}
