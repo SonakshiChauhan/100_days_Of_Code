6951fb5894e6d6fb3f56c2c926c3d42fcc780997
Sonakshi Chauhan <chauhansonakshi1402@gmail.com>
2022-09-19 23:36:34 +0530

    .

---
+++ src/com/company/Roman_to_Integer.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Roman_to_Integer {
+    public int romanToInt(String s) {
+        var letters = Map.of(
+
+                'I', 1,
+
+                'V', 5,
+
+                'X', 10,
+
+                'L', 50,
+
+                'C', 100,
+
+                'D', 500,
+
+                'M', 1000
+
+        );
+
+        int sum = 0;
+
+        int lastValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+
+            int val = letters.get(s.charAt(i));
+
+            sum = val >= lastValue ? sum + val : sum - val;
+
+            lastValue = val;
+
+        }
+
+        return sum;
+    }
+}
