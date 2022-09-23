99e423fee050351958eb086d733c805fdcd826cc
Sonakshi Chauhan <chauhansonakshi1402@gmail.com>
2022-09-23 21:07:45 +0530

    .

---
+++ src/com/company/shifting_letters.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class shifting_letters {
+
+        public String shiftingLetters(String S, int[] shifts) {
+            StringBuilder ans = new StringBuilder();
+            int X = 0;
+            for (int shift: shifts)
+                X = (X + shift) % 26;
+
+            for (int i = 0; i < S.length(); ++i) {
+                int index = S.charAt(i) - 'a';
+                ans.append((char) ((index + X) % 26 + 97));
+                X = Math.floorMod(X - shifts[i], 26);
+            }
+
+            return ans.toString();
+        }
+    }
+
